1a1b67eb4a4a7c42edbe5523e471eea08cb30baa
Prajwalrh <106173137+Prajwalrh@users.noreply.github.com>
2023-05-20 18:43:23 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<html>
+<title>Golden Horizon Bank </title>
+<head>
+   <link rel="stylesheet" type="text/css" href="css/index.css">
+   <link rel="shortcut icon" href="img/images.jpg">
+    </head>
+ 
+<body>
+<?php include'header.php'?>
+<div class="index_container">
+    <div class="slider">
+    <div class="slideimg">
+    <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        </div>
+    </div><br>
+    <!-- <div class="newsroom">
+        
+        <marquee class="marquee_news" scrolldelay="20"><p class="newsfeed"><span>Your bank may charge you for closing a bank account. 
+        Bank also charges you when you close your account within a particular time period.</span><span>SBI cuts deposit rates; PPF to fetch lower interest rate. </span><span>No, it is not mandatory to link your bank account with CITIZENSHIP card</span></p></marquee>
+    </div><br><br> -->
+   
+
+    
+<div class="news_events">
+    <h4>Tips | Updates | Events</h4><br>
+        <ul>
+            <p>First, open an account. Then apply for a debit card to get further details.
+ </p><br>
+<p>And finally, proceed for Internet Banking Registration to create your internet banking account.
+
+</p>
+            <br>
+        </ul>
+    </div>
+    
+
+    <div class="online_services">
+        <h4>Online Services</h4>
+        <ul>
+            <a href="customer_reg_form.php"><li>Open Account</li></a>
+            <a href="debit_card_form.php"><li>Apply Debit Card</li></a><br>
+            <a href="#" id="ebanking" ><li><div class="ebanking">Internet Banking
+                <div class = "ebanking_options">
+                <ul>
+                    <a href="customer_login.php"><li>Login </li></a>
+                    <a href="ebanking_reg_form.php"><li>Register</li></a>
+                </ul>
+            </div>
+        </div>
+    </li></a>
+            <a href="fund_transfer.php"><li>Fund Transfer</li></a><br>
+        </ul>
+   
+</div>
+
+        <div id="aboutus" class="about"><span>About Us</span><br><br>
+        <p>Golden Horizon Bank a Fortune 500 company, is an Indian Multinational, Public Sector Banking and Financial services statutory body headquartered in Mumbai. The rich heritage and legacy of over 200 years, accredits  as the most trusted Bank by Indians through generations.
+Golden Horizon Bank , the largest Indian Bank with 1/4th market share, serves over 45 crore customers through its vast network of over 22,000 branches, 62617 ATMs/ADWMs, 71,968 BC outlets, with an undeterred focus on innovation, and customer centricity, which stems from the core values of the Bank - Service, Transparency, Ethics, Politeness and Sustainability.
+The Bank has successfully diversified businesses through its various subsidiaries i.e General Insurance, Life Insurance, Mutual Fund, Card, etc. It has spread its presence globally and operates across time zones through 229 offices in 31 foreign countries.
+
+</p></div>
+    
+    <div class="disclaimer">
+    <span>Disclaimer !!</spasn><br><br>
+        <p>Our bank does not ask for the details of your account/PIN/password. Therefore any one pretending to be asking you for information from the bank/technical team may be fraudulent entities, so please beware.</p>
+        <p>You should know how to operate net transactions and if you are not familiar you may refrain from doing so. You may seek bank's guidance in this regard. Bank is not responsible for online transactions going wrong.</p>
+        <p>We shall also not be responsible for wrong transactions and wanton disclosure of details by you. Viewing option and transaction option on the net are different. You may exercise your option diligently.</p>
+    </div>
+
+
+    </div>
+    
+<?php include 'footer.php';?>
+</body>
+
+</html>
